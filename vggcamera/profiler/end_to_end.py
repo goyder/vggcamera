44b5921f14b6a981eff292c5582267709be22971
Goyder <joshua@goyder.net.au>
2017-11-12 22:56:35 +0800

    Add VGG nano model

--- vggcamera/profiler/end_to_end.py
+++ vggcamera/profiler/end_to_end.py
@@ -214,6 +214,10 @@ def create_vgg_nano():
     model = Sequential()
 
     model.add(ZeroPadding2D((1, 1), input_shape=(3,224,224)))
+    model.add(Convolution2D(32, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2, 2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1, 1)))
     model.add(Convolution2D(64, 3, 3, activation='relu'))
     model.add(MaxPooling2D((2, 2), strides=(2,2)))
 
@@ -226,9 +230,9 @@ def create_vgg_nano():
     model.add(MaxPooling2D((2, 2), strides=(2,2)))
 
     model.add(ZeroPadding2D((1, 1)))
-    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
     model.add(MaxPooling2D((2, 2), strides=(2,2)))
-
+    
     model.add(Flatten())
 
     model.add(Dense(2048, activation='relu'))
